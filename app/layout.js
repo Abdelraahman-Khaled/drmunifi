881d2acb4f3932b0d6c2c155999dc3041a1a789b
Abdelrahman Khaled <109894754+Abdelraahman-Khaled@users.noreply.github.com>
2026-02-16 19:47:33 +0200

    update gtm

--- app/layout.js
+++ app/layout.js
@@ -8,102 +8,129 @@ import MagicCursor from "./components/MagicCursor";
 import Script from "next/script";
 
 export const metadata = {
-    metadataBase: new URL('https://dralmunifi.com'),
-    title: {
-        default: "الدكتور عبدالله المنيفي - جراحة السمنة والمناظير",
-        template: "%s | الدكتور عبدالله المنيفي"
+  metadataBase: new URL("https://almunifi.com"),
+  title: {
+    default: "الدكتور عبدالله المنيفي - جراحة السمنة والمناظير",
+    template: "%s | الدكتور عبدالله المنيفي",
+  },
+  description:
+    "الدكتور عبدالله المنيفي استشاري جراحة السمنة والمناظير المتقدمة. متخصص في تكميم المعدة، تحويل المسار، والحلول الجراحية الدقيقة للسمنة في المملكة العربية السعودية.",
+  keywords: [
+    "جراحة السمنة",
+    "تكميم المعدة",
+    "تحويل المسار",
+    "دكتور عبدالله المنيفي",
+    "Bariatric Surgery",
+    "Gastric Sleeve",
+    "Saudi Arabia",
+  ],
+  authors: [{ name: "Dr. Abdullah AlMunifi" }],
+  creator: "Dr. Abdullah AlMunifi",
+  publisher: "Dr. Abdullah AlMunifi",
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
     },
-    description: "الدكتور عبدالله المنيفي استشاري جراحة السمنة والمناظير المتقدمة. متخصص في تكميم المعدة، تحويل المسار، والحلول الجراحية الدقيقة للسمنة في المملكة العربية السعودية.",
-    keywords: ["جراحة السمنة", "تكميم المعدة", "تحويل المسار", "دكتور عبدالله المنيفي", "Bariatric Surgery", "Gastric Sleeve", "Saudi Arabia"],
-    authors: [{ name: "Dr. Abdullah AlMunifi" }],
-    creator: "Dr. Abdullah AlMunifi",
-    publisher: "Dr. Abdullah AlMunifi",
-    robots: {
-        index: true,
-        follow: true,
-        googleBot: {
-            index: true,
-            follow: true,
-            'max-video-preview': -1,
-            'max-image-preview': 'large',
-            'max-snippet': -1,
-        },
-    },
-    openGraph: {
-        type: 'website',
-        locale: 'ar_SA',
-        alternateLocale: 'en_US',
-        url: 'https://dralmunifi.com',
-        siteName: 'Dr. Abdullah AlMunifi',
-        title: "الدكتور عبدالله المنيفي - خيارك الأول لجراحة السمنة",
-        description: "استشاري جراحة السمنة والمناظير المتقدمة. نقدم رعاية طبية متكاملة لمرضى السمنة بأحدث التقنيات.",
-        images: [
-            {
-                url: '/assets/img/about-img1.webp', // Using the about image as a fallback OG image
-                width: 1200,
-                height: 630,
-                alt: 'Dr. Abdullah AlMunifi',
-            },
-        ],
-    },
-    twitter: {
-        card: 'summary_large_image',
-        title: "الدكتور عبدالله المنيفي",
-        description: "استشاري جراحة السمنة والمناظير المتقدمة.",
-        images: ['/assets/img/about-img1.webp'],
-        creator: '@DrAlMunifi', // Assuming finding this handle from the Navbar link
-    },
-    icons: {
-        icon: '/assets/img/favicon.png',
-        shortcut: '/assets/img/favicon.png',
-        apple: '/assets/img/favicon.png',
-    },
+  },
+  openGraph: {
+    type: "website",
+    locale: "ar_SA",
+    alternateLocale: "en_US",
+    url: "https://dralmunifi.com",
+    siteName: "Dr. Abdullah AlMunifi",
+    title: "الدكتور عبدالله المنيفي - خيارك الأول لجراحة السمنة",
+    description:
+      "استشاري جراحة السمنة والمناظير المتقدمة. نقدم رعاية طبية متكاملة لمرضى السمنة بأحدث التقنيات.",
+    images: [
+      {
+        url: "/assets/img/about-img1.webp", // Using the about image as a fallback OG image
+        width: 1200,
+        height: 630,
+        alt: "Dr. Abdullah AlMunifi",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "الدكتور عبدالله المنيفي",
+    description: "استشاري جراحة السمنة والمناظير المتقدمة.",
+    images: ["/assets/img/about-img1.webp"],
+    creator: "@DrAlMunifi", // Assuming finding this handle from the Navbar link
+  },
+  icons: {
+    icon: "/assets/img/favicon.png",
+    shortcut: "/assets/img/favicon.png",
+    apple: "/assets/img/favicon.png",
+  },
 };
 
-
-
-
 export default function RootLayout({ children }) {
-    // Note: Since this is a server component, we use ar as default.
-    // The LanguageProvider will update document attributes on the client.
-    return (
-        <html lang="ar" dir="rtl" suppressHydrationWarning>
-            <head>
-                <link
-                    rel="stylesheet"
-                    href="https://cdn-uicons.flaticon.com/3.0.0/uicons-regular-rounded/css/uicons-regular-rounded.css"
-                />
-                <link
-                    rel="stylesheet"
-                    href="https://cdn-uicons.flaticon.com/3.0.0/uicons-regular-straight/css/uicons-regular-straight.css"
-                />
-                <link rel="icon" type="image/png" href="/assets/img/favicon.png" />
-                <link rel="stylesheet" href="/assets/css/style.css" />
-                <link rel="stylesheet" href="/assets/css/style-ar.css" />
-            </head>
-            <body>
-                <LanguageProvider initialLanguage="ar">
-                    <MagicCursor />
-                    <Preloader />
-                    <Navbar />
-                    {children}
-                    <Footer />
+  // Note: Since this is a server component, we use ar as default.
+  // The LanguageProvider will update document attributes on the client.
+  return (
+    <html lang="ar" dir="rtl" suppressHydrationWarning>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdn-uicons.flaticon.com/3.0.0/uicons-regular-rounded/css/uicons-regular-rounded.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdn-uicons.flaticon.com/3.0.0/uicons-regular-straight/css/uicons-regular-straight.css"
+        />
+        <link rel="icon" type="image/png" href="/assets/img/favicon.png" />
+        <link rel="stylesheet" href="/assets/css/style.css" />
+        <link rel="stylesheet" href="/assets/css/style-ar.css" />
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-WTPCCCX9');`}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WTPCCCX9"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
 
-                    {/* Scripts */}
-                    <Script src="/assets/js/jquery.min.js" strategy="beforeInteractive" />
-                    <Script src="/assets/js/bootstrap.bundle.min.js" />
-                    <Script src="/assets/js/owl.carousel.min.js" />
-                    <Script src="/assets/js/slick.min.js" />
-                    <Script src="/assets/js/jquery.magnific-popup.min.js" />
-                    <Script src="/assets/js/jquery.nice-select.min.js" />
-                    <Script src="/assets/js/jquery.meanmenu.js" />
-                    <Script src="/assets/js/jquery.ajaxchimp.min.js" />
-                    <Script src="/assets/js/parallax.min.js" />
-                    <Script src="/assets/js/jquery.appear.min.js" />
-                    <Script src="/assets/js/wow.min.js" />
-                    <Script src="/assets/js/main.js" strategy="lazyOnload" />
-                </LanguageProvider>
-            </body>
-        </html>
-    );
+        <LanguageProvider initialLanguage="ar">
+          <MagicCursor />
+          <Preloader />
+          <Navbar />
+          {children}
+          <Footer />
+
+          {/* Scripts */}
+          <Script src="/assets/js/jquery.min.js" strategy="beforeInteractive" />
+          <Script src="/assets/js/bootstrap.bundle.min.js" />
+          <Script src="/assets/js/owl.carousel.min.js" />
+          <Script src="/assets/js/slick.min.js" />
+          <Script src="/assets/js/jquery.magnific-popup.min.js" />
+          <Script src="/assets/js/jquery.nice-select.min.js" />
+          <Script src="/assets/js/jquery.meanmenu.js" />
+          <Script src="/assets/js/jquery.ajaxchimp.min.js" />
+          <Script src="/assets/js/parallax.min.js" />
+          <Script src="/assets/js/jquery.appear.min.js" />
+          <Script src="/assets/js/wow.min.js" />
+          <Script src="/assets/js/main.js" strategy="lazyOnload" />
+        </LanguageProvider>
+      </body>
+    </html>
+  );
 }
